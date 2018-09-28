09a08ea5d1f74d45cce61da36d1c4bd994983295
Flodersdsdsdsdsd <43557824+Flodersdsdsdsdsd@users.noreply.github.com>
2018-09-28 17:37:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ['-'];
-
+fd
 client.on('ready', () => {
   console.log('======================================')
   console.log(`Logged in as ${client.user.tag}!`);
